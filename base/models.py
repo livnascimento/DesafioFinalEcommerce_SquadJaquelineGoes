f8728e0e64fba8cb7c7c14393e0c55be68b60b74
Lívia <liviapnd@gmail.com>
2024-03-22 18:14:50 -0300

    feat: model Carrinho

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from base.classes import Item
 
 class Produtos(models.Model):
     titulo = models.CharField(max_length=50, null=False)
@@ -6,6 +7,9 @@ class Produtos(models.Model):
     imgUrl = models.CharField(max_length=1000,null=False)
     preco = models.FloatField(null=False)
     qtdEstoque = models.IntegerField(null=False)
+
+class Carrinho(models.Model):
+    produtos = models.JSONField()
 
 class Pedidos(models.Model):
     data = models.DateTimeField(auto_now_add=True)
