b7c0edef44733ba9c94346c6f047e488eac165d3
Lívia <liviapnd@gmail.com>
2024-03-22 15:20:38 -0300

    build: adicionando as models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produtos(models.Model):
+    titulo = models.CharField(max_length=50, null=False)
+    descricao = models.CharField(max_length=255, null=False)
+    imgUrl = models.CharField(max_length=1000,null=False)
+    preco = models.FloatField(null=False)
+    qtdEstoque = models.IntegerField(null=False)
+
+class Pedidos(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    produtos = models.JSONField()
