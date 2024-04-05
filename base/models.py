4d66a89bbf7fe27b3b1b4cae8402e7468bff05dd
Edryel2 <adrielly70@hotmail.com>
2024-04-04 21:36:05 -0300

    Cliente, Login, Categoria e Pesquisa

--- base/models.py
+++ base/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
-    image = models.ImageField(upload_to='images/', default='default/path/to/image.jpg')
+    image = models.ImageField(upload_to='uploads/product/')
 
   
 
