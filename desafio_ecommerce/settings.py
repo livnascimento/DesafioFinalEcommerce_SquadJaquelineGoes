b769ea078207f4872d0a3f35e0bb3fae4dedae30
Lívia <liviapnd@gmail.com>
2024-04-12 19:21:27 -0300

    build: update no settings.py

--- desafio_ecommerce/settings.py
+++ desafio_ecommerce/settings.py
@@ -121,10 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['/static/']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
