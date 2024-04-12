247f6ae91e94946f70033eefa8d7dda7665f4a38
Lívia <liviapnd@gmail.com>
2024-04-12 19:44:23 -0300

    styles: correção do caminho static

--- desafio_ecommerce/settings.py
+++ desafio_ecommerce/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-1#38w!h%8$fmq1tf6&(r5u)a^n*%xz8@s2c0@7njjudf$3_*+t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nerdwear-c24f5afb9888.herokuapp.com']
+ALLOWED_HOSTS = [
+                    'nerdwear-c24f5afb9888.herokuapp.com',
+                    '127.0.0.1'
+                ]
 
 
 # Application definition
